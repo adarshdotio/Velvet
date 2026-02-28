355352aea0b4aa66333ce31e814afb2947e9b435
adarshdotio <urstrulyadxrsh@gmail.com>
2026-02-28 19:46:24 +0530

    added some backend stuff

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePic: {
+    type: String,
+    default: ''
+  }
+}, { timestamps: true });
+
+const userModel = mongoose.model('model', userSchema);
+
+export default userModel;
---
+++ node_.md
@@ -0,0 +1,3 @@
+```bash
+cd backend && npm i express jsonwebtoken mongoose cloudinary bcryptjs socket.io cookie-parser dotenv
+```
