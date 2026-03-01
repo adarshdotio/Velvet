9807a4865c07962f642e285cc500bd067d053ef0
adarshdotio <urstrulyadxrsh@gmail.com>
2026-03-01 16:23:53 +0530

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,21 +1,53 @@
+import bcrypt from 'bcryptjs';
+import generateToken from '../lib/utils.js';
 import User from '../models/user.model.js';
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    if (password.length < 6) {
-      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required.' });
     }
-    const isEmailExists = await User;
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long.' });
+    }
+    const isEmailExists = await User.findOne({ email });
+    if (isEmailExists) {
+      return res.status(400).json({ message: 'Email already exists.' });
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword
+    });
+    
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        id: newUser._id,
+        fullName,
+        email,
+        profilePic: newUser.profilePic
+      })
+    } else {
+      return res.status(400).json({ message: 'Error creating account.' })
+    }
+    
   } catch (e) {
-    throw e
+    res.status(500).json({ message: 'Internal Server Error.' });
+    console.log('Error in signup controller', e.message);
   }
 };
 
-export const signup = (req, res) => {
+export const login = (req, res) => {
   res.send('signup api');
 };
 
-export const signup = (req, res) => {
+export const logout = (req, res) => {
   res.send('signup api');
 };
